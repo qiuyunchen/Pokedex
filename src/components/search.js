d3b22dc19c84e2fd84326b5f80ecd8d359618e24
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-02-12 11:04:15 -0500

    added filterDropdown function and preliminary drop-down-box js logic and css

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,33 @@
 
 import React from 'react';
-import './search.css'
+import './search.css';
+import SearchList from './searchList';
+let dropDownList = [];
+
+const filterDropdown = (e) => {
+    const searchStr = e.target.value.trim().toLowerCase();
+    const l = searchStr.length;
+    dropDownList = SearchList.filter(n => n.toLowerCase().slice(0,l) === searchStr);
+    
+    console.log('dropdown list...', dropDownList)
+    console.log('class list...', document.querySelector('#dropdown').classList);
+    //document.querySelector('#dropdown').classList.remove('hidden');
+}
 
 const Search = () => {
-    return <input className='search_bar' type='text' placeholder='Search...'></input>;
+    const hiddenClassList = 'dropdown_box hidden';
+
+    return (
+        <div>
+            <input className='search_bar' onChange={e => filterDropdown(e)} type='text' placeholder='Search...'></input>
+            {/*  */}
+            <div id='dropdown' className={dropDownList.length > 0 ? 'dropdown_box' : hiddenClassList}>
+
+            </div>
+            {/*  */}
+        </div>
+    );
+    
 };
 
 export default Search;
-

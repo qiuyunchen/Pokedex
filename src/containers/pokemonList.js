b79c9e166ad79bf2c2cd3b8fb1cc4856dd582035
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-02-12 07:52:29 -0500

    added pokemon container min-width, cleaned up code, corrected naming typos

--- src/containers/pokemonList.js
+++ src/containers/pokemonList.js
@@ -4,20 +4,18 @@ import './pokemonList.css'
 
 const List = (props) => {
     return (
-        <div className='pknm-container'>
-        <h2>POKEMON CONTAINER</h2>
-        <div className='pkmn-list'>
-            {
-                props.list.map((e, i) => {
-                    // console.log(i)
-                    return (
-                        <div className="pkmn" key={i} style={{color:'blue', padding:'10px'}}>
-                        <Pokemon name={e.name} index={i} click={props.click}/>
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <div className='pkmn-container'>
+            <div className='pkmn-list'>
+                {
+                    props.list.map((e, i) => {
+                        return (
+                            <div className="pkmn" key={i}>
+                                <Pokemon name={e.name} index={i} click={props.click}/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 };

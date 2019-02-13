ced96c7747390bb1531f8a0085d7298492bb560b
Syed Aziz <syedaziz27x@gmail.com>
2019-02-12 20:32:06 -0500

    add componentDidMount to App.js

--- src/App.js
+++ src/App.js
@@ -14,29 +14,8 @@ class App extends Component {
     super(props)
 
     this.state = {
-      list: [
-        {name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"}, // url holds pkmns move and abilities
-        {name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/"},
-        {name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/"},
-        {name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/"},
-        {name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/"},
-        {name: "charizard", url: "https://pokeapi.co/api/v2/pokemon/6/"},
-        {name: "squirtle", url: "https://pokeapi.co/api/v2/pokemon/7/"},
-        {name: "wartortle", url: "https://pokeapi.co/api/v2/pokemon/8/"},
-        {name: "blastoise", url: "https://pokeapi.co/api/v2/pokemon/9/"},
-        {name: "caterpie", url: "https://pokeapi.co/api/v2/pokemon/10/"},
-        {name: "metapod", url: "https://pokeapi.co/api/v2/pokemon/11/"},
-        {name: "butterfree", url: "https://pokeapi.co/api/v2/pokemon/12/"},
-        {name: "weedle", url: "https://pokeapi.co/api/v2/pokemon/13/"},
-        {name: "kakuna", url: "https://pokeapi.co/api/v2/pokemon/14/"},
-        {name: "beedrill", url: "https://pokeapi.co/api/v2/pokemon/15/"},
-        {name: "pidgey", url: "https://pokeapi.co/api/v2/pokemon/16/"},
-        {name: "pidgeotto", url: "https://pokeapi.co/api/v2/pokemon/17/"},
-        {name: "pidgeot", url: "https://pokeapi.co/api/v2/pokemon/18/"},
-        {name: "rattata", url: "https://pokeapi.co/api/v2/pokemon/19/"},
-        {name: "raticate", url: "https://pokeapi.co/api/v2/pokemon/20/"},
-    ],
-    offset: 20,
+      list: [],
+    offset: 0,
     clickedPokemon: false,
     pokemonClicked : null,
     pokemonClickedUrl: null,
@@ -58,6 +37,8 @@ class App extends Component {
       console.log(this.state.pokemonClicked, 'was clicked!');
     });
   }
+
+
   loadMore = () => {
     Axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${this.state.offset}&limit=20`)
     .then((data) => {
@@ -79,9 +60,13 @@ class App extends Component {
 
   checkPokemon = (e) => {
     console.log(e.target.getAttribute('url'))
-    this.setState({clickedPokemon: false, pokemonClicked: e.target.getAttribute('value'), pokemonClickedUrl: e.target.getAttribute('url')}, ()=> {
+    this.setState({clickedPokemon: true, pokemonClicked: e.target.getAttribute('value'), pokemonClickedUrl: e.target.getAttribute('url')}, ()=> {
       console.log(this.state)
     })
+  }
+
+  componentDidMount() {
+    this.loadMore();
   }
  
   render() {

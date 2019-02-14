a337bd8b3cf0c7205b3dcc63935b6482532a2beb
Syed Aziz <syedaziz27x@gmail.com>
2019-02-14 01:27:46 -0500

    uppercased name

--- src/components/homeComponents/pokemon.js
+++ src/components/homeComponents/pokemon.js
@@ -9,12 +9,14 @@ const padNum = (num) => {
 
 const Pokemon = (props) => {
     let num = props.index + 1;
+    let name1 = props.name;
+    name1 = props.name.charAt(0).toUpperCase() + props.name.slice(1);
 
     return (
         <div className="pkmn-row" onClick={props.click} value={props.name} url={props.url}>
             <div value={props.name} url={props.url}>
                 <img className="mini-pkmn-img" value={props.name} url={props.url} src={`https://img.pokemondb.net/sprites/sun-moon/icon/${props.name}.png`} alt={'idk'} />
-                {props.name}
+                {name1}
             </div>
             <div value={props.name} url={props.url}> # {padNum(num)} </div>
         </div>
@@ -22,3 +24,6 @@ const Pokemon = (props) => {
 }
 
 export default Pokemon;
+
+
+
